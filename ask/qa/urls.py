9c2f468bf375e8a2c3aae0e272f4110aee864175
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-18 23:45:46 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url, include
 
+from . import views
+
 
 urlpatterns = [                                             
-   url(r'^$', 'views.test'),                                                              
-   url(r'^login/.*$', 'views.test', name='login'),                                    
-   url(r'^signup/.*', 'views.test', name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', 'views.test', name='question'),                 
-   url(r'^ask/.*', 'views.test', name='ask'),                                         
-   url(r'^popular/.*', 'views.test', name='popular'),                                 
-   url(r'^new/.*', 'views.test', name='new'),                                         
+   url(r'^$', views.test),                                                              
+   url(r'^login/.*$', views.test, name='login'),                                    
+   url(r'^signup/.*', views.test, name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', views.test, name='question'),                 
+   url(r'^ask/.*', views.test, name='ask'),                                         
+   url(r'^popular/.*', views.test, name='popular'),                                 
+   url(r'^new/.*', views.test, name='new'),                                         
 ] 
