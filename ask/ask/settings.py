20e6a13e107f9e923b3809a07dbc9839844dea08
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-18 22:23:26 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$plb9+((vcvzxf=mgfxmx5lmd=svieiu^98(*%6**je0q)=+oo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
