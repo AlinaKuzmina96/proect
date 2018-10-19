07087f986041d61b745ff9aa9aea5e2b6e535a79
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-19 23:22:38 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
+sudo /etc/init.d/gunicorn restart
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django.conf
 sudo /etc/init.d/gunicorn restart
