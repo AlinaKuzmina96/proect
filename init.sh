c779812635c73f6ec3bdc7b200076df32d542540
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-22 22:26:19 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-sudo rm -r /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-sudo rm -r /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application &
