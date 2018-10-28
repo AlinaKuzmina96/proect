12a26e676570b001944e0beffbf139554e111be8
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-28 23:24:32 +0300

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-def app(environ, start_response):
-  body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-        
-  status = "200 Ok"
-  headers = [
-      ('Content-type', 'text/plain')
-  ]
-  start_response(status, headers)
-  return body
