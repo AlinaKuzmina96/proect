ef22719c1de9256c05c8636ae492247666a9fd2e
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-30 00:03:44 +0300

    Update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,9 +1,7 @@
-sudo rm -r /etc/mysql/my.cnf
-sudo ln -sf /home/box/web/etc/my.cnf  /etc/mysql/my.cnf
 sudo /etc/init.d/mysql start
-
-mysql -uroot -e "CREATE DATABASE qa"
-mysql -uroot -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY 'qapass';
-				 GRANT ALL ON qa.* TO 'qauser'@'localhost';"
+mysql -uroot -e "CREATE DATABASE djbase;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
+mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 
 sudo python /home/box/web/ask/manage.py syncdb
