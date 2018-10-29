0cc98a7a1690df63036f874cebe737d4b893383e
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-29 23:51:12 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+  title = models.CharField(default="", max_length=255)
+  text = models.TextField(default="")
+  added_ad = models.DateTimeField(null=True)
+  rating = models.IntegerField(default=0)
+  author = models.ForeignKey(User, related_name="question_author")
+  likes = models.ManyToManyField(
+        User, related_name="question_like", blank=True)
+
+  class Meta:
+        ordering = ('-added_at',)
+
+  def __str__(self):
+        return self.title
+
+  def get_absolute_url(self):
+        return reverse('question_detail', kwargs={'pk': self.pk})
+
+
+
+class Answer(models.Model):
+  text = models.TextField(default="")
+  added_ad = models.DateTimeField(null=True)
+  question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+  
+  def __str__(self):
+        return self.text
