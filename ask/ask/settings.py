c766364699ca212392b6c6e376d0b0016800cb7d
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-30 00:08:40 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -74,15 +74,14 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if os.name == "nt" or True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
+#if os.name == "nt" or True:
+    #DATABASES = {
+        #'default': {
+            #'ENGINE': 'django.db.backends.sqlite3',
+            #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #}
+    #}
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'djbase',
