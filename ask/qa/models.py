1a7c526ea9b846d80a78f4248c2a3285ab538157
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-30 21:28:37 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):                                          
+        def new(self):
+            return self.order_by('-added_at')
+        def popular(self):
+            return self.order_by('-rating')  
+          
 class Question(models.Model):
   objects = QuestionManager() 
   title = models.CharField(default="", max_length=255)
@@ -12,20 +18,12 @@ class Question(models.Model):
   likes = models.ManyToManyField(
         User, related_name="question_like", blank=True)
 
-  class Meta:
-        ordering = ('-added_at',)
-
   def __str__(self):
         return self.title
 
   def get_absolute_url(self):
         return reverse('question_detail', kwargs={'pk': self.pk})
-
-class QuestionManager(models.Manager):                                          
-        def new(self):
-            return self.order_by('-added_at')
-        def popular(self):
-            return self.order_by('-rating')                                               
+                                             
         
 
 class Answer(models.Model):
