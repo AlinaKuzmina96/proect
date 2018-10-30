7bb64a3bbf65439e408857c0cc297f1384fa5c2b
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-30 22:21:25 +0300

    Update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,2 +1,5 @@
 sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE djbase;"
+mysql -uroot -e "CREATE USER 'root'@'localhost'"
+mysql -uroot -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('pass111')"
+mysql -uroot -e "CREATE DATABASE djbase"
+mysql -uroot -e "GRANT ALL ON mybase.* TO 'root'@'localhost'"
