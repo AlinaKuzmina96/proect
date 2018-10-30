50d5a93882a315da427017c22e8daf20c3dbc42a
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-10-30 22:58:59 +0300

    Update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,3 +1,3 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE djbase;"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON djbase.* TO 'box'@'localhost' WITH GRANT OPTION;"
