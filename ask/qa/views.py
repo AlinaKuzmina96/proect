861defd39947f5cb9f492f36915e5e83f7467109
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-11-14 23:16:12 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 from django.http import HttpResponse 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+
+def paginate(request):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 2))
+    except ValueError:
+        raise Http404
+    paginator = Paginator(questions, limit)
+    page = paginator.page(page)
+    return render(request, 'list.html', {
+        'paginator': paginator,
+        'title': 'Latest',
+        'questions': page.object_list,
+        'page': page
+        })
+
+
+               
