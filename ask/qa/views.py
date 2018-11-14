cf5ffd4ac2c7b2c0f6a2f50aff4dc933d06348c5
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-11-14 23:23:57 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -7,7 +7,7 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 
-def paginate(request):
+def main_page(request):
     try:
         limit = int(request.GET.get('limit', 10))
     except ValueError:
@@ -18,6 +18,7 @@ def paginate(request):
         page = int(request.GET.get('page', 2))
     except ValueError:
         raise Http404
+    questions = Question.objects.all().order_by('-id')
     paginator = Paginator(questions, limit)
     page = paginator.page(page)
     return render(request, 'list.html', {
@@ -27,5 +28,24 @@ def paginate(request):
         'page': page
         })
 
-
+def popular(request):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 2))
+    except ValueError:
+        raise Http404
+    questions = Question.objects.all().order_by('-rating')
+    paginator = Paginator(questions, limit)
+    page = paginator.page(page)
+    return render(request, 'list.html', {
+        'paginator': paginator,
+        'title': 'Popular',
+        'questions': page.object_list,
+        'page': page
+        })
                
