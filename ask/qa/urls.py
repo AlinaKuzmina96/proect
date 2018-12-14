1495b5107b15d19f289587efffacfe0571643b0d
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-12-14 22:08:55 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 
 urlpatterns = [                                             
-   url(r'^$', views.test),                                                              
+   url(r'^$', views.main_page),                                                              
    url(r'^login/.*$', views.test, name='login'),                                    
    url(r'^signup/.*', views.test, name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', views.test, name='question'),                 
+   url(r'^question/(?P<id>[0-9]+)/$', views.question, name='question'),                 
    url(r'^ask/.*', views.test, name='ask'),                                         
-   url(r'^popular/.*', views.test, name='popular'),                                 
+   url(r'^popular/.*', views.popular, name='popular'),                                 
    url(r'^new/.*', views.test, name='new'),                                         
 ]
