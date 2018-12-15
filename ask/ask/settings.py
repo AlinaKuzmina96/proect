cff7d69b588e1955d5cd1c5052ff8a110e2cd7d4
AlinaKuzmina96 <42009903+AlinaKuzmina96@users.noreply.github.com>
2018-12-15 23:25:12 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i0c3fo1tgws4xyanz-e7!@-28qy%ewuq4x^hvhjq8e$477vaul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
